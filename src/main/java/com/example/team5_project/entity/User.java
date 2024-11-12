2d95e5c30fd846f6b1c20513636ec725ecb9913d
jeonghak <kjh47345@naver.com>
2024-11-12 17:13:40 +0900

    파일구조, 회원가입 validate추가

--- src/main/java/com/example/team5_project/entity/User.java
+++ src/main/java/com/example/team5_project/entity/User.java
@@ -1,9 +1,7 @@
 package com.example.team5_project.entity;
 
-import org.hibernate.annotations.ColumnDefault;
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,12 +16,23 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
+    // 이름은 한글, 영어, 숫자만 허용, 최대 10자 제한, 공백 불가
     @Column(unique = true)
     @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{1,10}$", message = "Name can only contain Korean, English letters, and numbers, and must be up to 10 characters.")
     private String name;
 
+    // 이메일은 이메일 형식 검사
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    // 패스워드는 한글, 영어, 숫자, 특수기호 포함 가능, 4-20자 제한
+    @NotEmpty(message = "Password cannot be empty")
+    @Size(min = 4, max = 20, message = "Password must be between 4 and 20 characters")
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9!@#\\$%\\^&\\*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?~`]+$",
+            message = "Password can contain Korean, English letters, numbers, and special characters")
     private String password;
+
     private Integer attendance;
 
     @Builder
@@ -34,4 +43,3 @@ public class User {
         this.attendance = attendance;
     }
 }
-
